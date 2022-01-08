21bcf55554fa71d22e426a056167e404bec0f4aa
mojotron <igormalj@gmail.com>
2022-01-08 20:45:58 +0100

    experiment with input eventListener for form validation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -63,3 +63,65 @@ mobileMenu.addEventListener('click', e => {
 
 // plug in imgae slider to the body
 ImageSlider(document.body, [image1, image2, image3]);
+
+// client form validation
+// valid email regexp from MDN
+// regexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const formElement = document.querySelector('.form');
+const username = formElement.querySelector('#username');
+// const userEmail = formElement.querySelector('#email');
+// const userPassword1 = formElement.querySelector('#password-1');
+// const userPassword2 = formElement.querySelector('#password-2');
+
+// validate empty string
+
+const validateUsername = value => {
+  const usernameRegex = /^[a-z_]*$/i;
+  if (value === '') return 'Username input filed is empty!';
+  if (!value.match(usernameRegex))
+    return 'Username can include letters and underscore!';
+  if (value.length < 4 || value.length > 15)
+    return 'Username must be between 5 and 15 characters long!';
+
+  return false;
+};
+
+formElement.addEventListener('submit', e => {
+  e.preventDefault();
+  const usernameCheck = validateUsername(username.value);
+  // console.log(usernameCheck);
+  if (usernameCheck) {
+    const errorEle = username.parentElement.querySelector(
+      '.form__field__error'
+    );
+    errorEle.innerText = usernameCheck;
+    errorEle.style.display = 'block';
+  }
+  // validateEmail();
+  // validatePassword();
+  // validatePasswordRetype();
+  // hideAllErrorMsgs();
+});
+
+// const hideAllErrorMsgs = () => {
+//   document.querySelectorAll('.form__field__error').forEach(errorEle => {
+//     errorEle.style.display = 'none';
+//   });
+// };
+
+formElement.querySelectorAll('input').forEach(input =>
+  input.addEventListener('input', e => {
+    if (e.target.matches('#username')) {
+      const usernameCheck = validateUsername(username.value);
+      const errorEle = username.parentElement.querySelector(
+        '.form__field__error'
+      );
+      if (usernameCheck) {
+        errorEle.innerText = usernameCheck;
+        errorEle.style.display = 'block';
+      } else {
+        errorEle.style.display = 'none';
+      }
+    }
+  })
+);
