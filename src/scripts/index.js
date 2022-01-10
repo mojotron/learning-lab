5a97a9b5d500b8f354980a847ce81535c8a6607c
mojotron <igormalj@gmail.com>
2022-01-10 22:21:47 +0100

    Move validate logic to and use input names to connect current input and validate function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -61,122 +61,94 @@ mobileMenu.addEventListener('click', e => {
     .classList.toggle('active-toggle');
 });
 
-// plug in imgae slider to the body
+// plug in image slider to the body
 ImageSlider(document.body, [image1, image2, image3]);
 
 // client form validation
-// valid email regexp from MDN
-// regexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 const formElement = document.querySelector('.form');
-const username = formElement.querySelector('#username');
-// const userEmail = formElement.querySelector('#email');
-// const userPassword1 = formElement.querySelector('#password-1');
-// const userPassword2 = formElement.querySelector('#password-2');
 
-// validate empty string
+const validateInput = {
+  username(value) {
+    const usernameRegex = /^[a-z_]*$/i;
+    if (value === '') return 'Username input filed is empty!';
+    if (!value.match(usernameRegex))
+      return 'Username can include letters and underscore!';
+    if (value.length < 4 || value.length > 15)
+      return 'Username must be between 4 and 15 characters long!';
+    return false;
+  },
 
-const validateUsername = value => {
-  const usernameRegex = /^[a-z_]*$/i;
-  if (value === '') return 'Username input filed is empty!';
-  if (!value.match(usernameRegex))
-    return 'Username can include letters and underscore!';
-  if (value.length < 4 || value.length > 15)
-    return 'Username must be between 5 and 15 characters long!';
+  email(value) {
+    const emailRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (value === '') return 'Email input field is empty';
+    if (!value.match(emailRegex))
+      return 'Please enter valid email form (example@email.com)!';
+    return false;
+  },
 
-  return false;
+  password(value) {
+    const charRegex = /[?=.*?[#?!@$%^&*-]/;
+    const upperCaseRegex = /[A-Z]/;
+    const lowerCaseRegex = /[a-z]/;
+    const numberRegex = /[0-9]/;
+    if (value === '') return 'Pease create password!';
+    if (value.length < 8) return 'password must be at least 8 characters long!';
+    if (value.length > 32) return 'Password maximum length is 32 characters!';
+    if (!value.match(charRegex))
+      return 'Password must include at least 1 special character!';
+    if (!value.match(upperCaseRegex))
+      return 'Password must include at least 1 upper case character!';
+    if (!value.match(lowerCaseRegex))
+      return 'Password must include at least 1 lower case character!';
+    if (!value.match(numberRegex))
+      return 'Password must include at least 1 numeric character!';
+    return false;
+  },
+
+  passwordRetype(value) {
+    if (value !== document.querySelector('#password-1').value)
+      return 'Passwords miss match';
+    return false;
+  },
 };
 
-const validateEmail = value => {
-  const emailRegex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (value === '') return 'Email input field is empty';
-  if (!value.match(emailRegex))
-    return 'Please enter valid email form (example@email.cim)!';
-  return false;
-};
-
-const validatePassword = value => {
-  const charRegex = /[?=.*?[#?!@$%^&*-]/;
-  const upperCaseRegex = /[A-Z]/;
-  const lowerCaseRegex = /[a-z]/;
-  const numberRegex = /[0-9]/;
-  if (value === '') return 'Pease create password!';
-  if (value.length < 8) return 'password must be at least 8 characters long!';
-  if (value.length > 32) return 'Password maximum length is 32 characters!';
-  if (!value.match(charRegex))
-    return 'Pasword must include at least 1 special character!';
-  if (!value.match(upperCaseRegex))
-    return 'Password must include at least 1 upper case character!';
-  if (!value.match(lowerCaseRegex))
-    return 'Password must include at least 1 lower case character!';
-  if (!value.match(numberRegex))
-    return 'Password must include at least 1 numeric character!';
-  return false;
-};
-
-const validatePasswordRetype = value => {
-  if (value !== document.querySelector('#password-1').value)
-    return 'Passwords miss match';
-  return false;
-};
-
-formElement.addEventListener('submit', e => {
-  e.preventDefault();
-  const usernameCheck = validateUsername(username.value);
-  // console.log(usernameCheck);
-  if (usernameCheck) {
-    const errorEle = username.parentElement.querySelector(
-      '.form__field__error'
-    );
-    errorEle.innerText = usernameCheck;
-    errorEle.style.display = 'block';
+const updateInputError = (validate, errorElement) => {
+  if (validate) {
+    errorElement.innerText = validate;
+    errorElement.classList.add('active');
+  } else {
+    errorElement.classList.remove('active');
   }
-  // validateEmail();
-  // validatePassword();
-  // validatePasswordRetype();
-  // hideAllErrorMsgs();
-});
+};
+const updateInputIcon = (validate, parentElement) => {
+  if (validate) {
+    parentElement.querySelector('.icon--error').classList.add('active');
+    parentElement.querySelector('.icon--ok').classList.remove('active');
+  } else {
+    parentElement.querySelector('.icon--error').classList.remove('active');
+    parentElement.querySelector('.icon--ok').classList.add('active');
+  }
+};
 
-// const hideAllErrorMsgs = () => {
-//   document.querySelectorAll('.form__field__error').forEach(errorEle => {
-//     errorEle.style.display = 'none';
-//   });
-// };
+const validateController = e => {
+  const validate = validateInput[e.target.name](e.target.value);
+  const errorEle = e.target.parentElement.querySelector('.form__field__error');
+  updateInputError(validate, errorEle);
+  updateInputIcon(validate, e.target.parentElement);
+};
 
 formElement.querySelectorAll('input').forEach(input =>
   input.addEventListener('input', e => {
-    if (e.target.matches('#username')) {
-      const usernameCheck = validateUsername(username.value);
-      const errorEle = username.parentElement.querySelector(
-        '.form__field__error'
-      );
-      if (usernameCheck) {
-        errorEle.innerText = usernameCheck;
-        errorEle.style.display = 'block';
-      } else {
-        errorEle.style.display = 'none';
-      }
-    }
-    if (e.target.matches('#email')) {
-      const emailCheck = validateEmail(e.target.value);
-      console.log(e.target.value);
-      console.log(emailCheck);
-    }
-    if (e.target.matches('#password-1')) {
-      const passwordCheck = validatePassword(e.target.value);
-      const errorEle = e.target.parentElement.querySelector(
-        '.form__field__error'
-      );
-      if (passwordCheck) {
-        errorEle.innerText = passwordCheck;
-        errorEle.style.display = 'block';
-      } else {
-        errorEle.style.display = 'none';
-      }
-    }
-    if (e.target.matches('#password-2')) {
-      const passwordRetypeCheck = validatePasswordRetype(e.target.value);
-      console.log(passwordRetypeCheck);
+    if (
+      e.target.matches('#username') ||
+      e.target.matches('#email') ||
+      e.target.matches('#password-1') ||
+      e.target.matches('#password-2')
+    ) {
+      validateController(e);
     }
   })
 );
+
+// TODO add check on form submit
